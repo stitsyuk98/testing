4944441619a69d6e8a0f99d268c8b85238187689
Надежда Стицюк <stitsyuk98@mail.ru>
2020-10-24 22:44:42 +0500

    Update start.py
    отправляет сообщение и сохраняет переписку в бд

--- start.py
+++ start.py
@@ -1,10 +1,54 @@
+import pymongo
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import sqlite3
 
+def init_db():
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client['testing']
+    series_collection = db['msg_from_vk']
+    return series_collection
+
+def add_BD(series_collection, name, date, msg):
+    # client = pymongo.MongoClient('localhost', 27017)
+    # db = client['testing']
+    # series_collection = db['msg_from_vk']
+    series_collection.insert_one({"data": date, "user": name, "msg": msg}).inserted_id
+
+
+def last_msg_date(l_msg):
+    #time = l_msg.find_element_by_class_name("_im_mess_link")
+    #print(time.text)
+    date_li = l_msg.find_element_by_tag_name("li")
+    date_unix = date_li.get_attribute("data-ts")
+    print(date_unix)
+    return date_unix
+
+def last_msg_name(l_msg):
+    text = l_msg.find_element_by_class_name("im-mess-stack--lnk")
+    print(text.text)
+    return text.text
+
+def last_msg_text(l_msg):
+    text = l_msg.find_element_by_class_name("im-mess--text")
+    print(text.text)
+    return text.text.lower()
+
+#взвращаетэоемент содержащий текст, фото, имя, дату
+def last_msg(driver):
+    # WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("im_editable0"))
+    last_msg = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]')
+    # last_msg = driver.find_element_by_xpath('/html/body/div[11]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]/ul/li/div[3]')
+    # print(last_msg.get_attribute('innerHTML'))
+    # text = last_msg.find_element_by_class_name("im-mess--text")
+    # return last_msg.text.lower()
+    # print(text.text)
+    return last_msg
 
 def write(driver, text):
     msg_aria = driver.find_element_by_id("im_editable0")
@@ -13,22 +57,22 @@ def write(driver, text):
 def send(driver):
     driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[4]/div[2]/div[4]/div[1]/button').click()
 
-def last_msg(driver):
-    last_msg = driver.find_element_by_xpath('/html/body/div[11]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]/ul/li/div[3]')
-    print(last_msg.get_attribute('innerHTML'))
-    return last_msg.text.lower()
+# def last_msg(driver):
+#     last_msg = driver.find_element_by_xpath('/html/body/div[11]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]/ul/li/div[3]')
+#     print(last_msg.get_attribute('innerHTML'))
+#     return last_msg.text.lower()
 
 def main():
     with webdriver.Chrome() as driver:
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 20)
         driver.get("https://vk.com/")
         form = driver.find_element_by_id("index_login_form")
         email = form.find_element_by_id("index_email")
-        email.send_keys("*******")
+        email.send_keys("89002819280")
         login = form.find_element_by_id("index_pass")
-        login.send_keys("*************")
+        login.send_keys("vozvrat1202")
         form.find_element_by_id("index_login_button").click()
-        name = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_class_name("top_profile_name"))
+        name = wait.until(lambda x: x.find_element_by_class_name("top_profile_name"))
                                                # .find_element_by_id(“someId”))
         print(driver.current_url)
         # name = driver.find_element_by_class_name("top_profile_name")
@@ -36,59 +80,80 @@ def main():
 
         #click on message
         driver.find_element_by_id("l_msg").click()
-        name_pg = WebDriverWait(driver, 10).until(lambda x: x.find_elements_by_xpath('//span[contains(text(),"Павел Киселев")]'))
-
+        name_pg = wait.until(lambda x: x.find_element_by_xpath('//span[contains(text(),"Павел Киселев")]'))
+        print("i put it")
         #click on user
-        driver.find_element_by_xpath('//span[contains(text(),"Павел Киселев")]').click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("im_editable0"))
+        print(name_pg)
+        time.sleep(2)
+        name_pg.click()
+        # webdriver.ActionChains(driver).move_to_element(name_pg).click(name_pg).perform()
+        # driver.find_element_by_xpath('//span[contains(text(),"Павел Киселев")]').click()
+        # driver.find_element_by_xpath('//span[contains(text(),"Павел Киселев")]').click()
+        element = wait.until(expected_conditions.presence_of_element_located((By.TAG_NAME, "html")))
+        wait.until(lambda x: x.find_element_by_class_name("im-page--history-new-bar"))
 
-        driver.implicitly_wait(10)
 
 
+        time.sleep(2)
 
-        # driver.find_element_by_id("im_editable0").send_keys("я готов")
+        # client = pymongo.MongoClient('localhost', 27017)
+        # db = client['testing']
+        # series_collection = db['msg_from_vk']
 
-        # f = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[25]/div[2]/ul/li')
-        # last_msg = driver.find_element_by_xpath('/html/body/div[11]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]/ul/li/div[3]')
-        # print(last_msg.get_attribute('innerHTML'))
-
-        # msg_aria = driver.find_element_by_id("im_editable0")
-        # msg_aria.send_keys("давай попробуем")
-
-        # msg_text = last_msg.text.lower()
-        # WebDriverWait.until(expected_conditions.text_to_be_present_in_element(By.XPATH('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[19]/div[2]/ul/li/div[3]'), ""))
-        # if msg_text !="":
-        #     if msg_text == "да":
-        #         msg_aria.send_keys("ок")
-        #     elif msg_text == "нет":
-        #         msg_aria.send_keys("хорошо")
-
+        # l_msg = last_msg(driver)
+        # l_name = last_msg_name(l_msg)
+        # l_date_unix = last_msg_date(l_msg)
+        # l_msg_text = last_msg_text(l_msg)
+        print("i wait it")
+        # time.sleep(2)
+        # print(series_collection.insert_one({"data": l_date_unix, "user": l_name, "msg": l_msg_text}).inserted_id)
+        series_collection = init_db()
+        # add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+        # init_BD(l_name, l_date_unix, l_msg)
+        # driver.implicitly_wait(10)
+        write(driver, "готов")
+        send(driver)
         f = 0
         fl = False
-        while f<=6:
-            last_msg = driver.find_element_by_xpath('/html/body/div[11]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div/div/div[2]/div/div[1]/div[last()]/div[2]/ul/li/div[3]')
-            print(last_msg.get_attribute('innerHTML'))
-            m = last_msg.text.lower()
-            if m == "да":
-                write(driver, "ок")
-                send(driver)
-                fl = True
-            elif m == "нет":
-                write(driver, "хорошо")
-                send(driver)
-                fl = True
-            # else:
-            #     write("что?")
-            time.sleep(30)
+        date = 0
+        while f<=200:
+            l_msg = last_msg(driver)
+            l_date_unix = last_msg_date(l_msg)
+            l_msg_text = last_msg_text(l_msg)
+            l_name = last_msg_name(l_msg)
+            if date != l_date_unix & l_name != "Надежда":
+                if l_msg_text == "да":
+                    write(driver, "ок")
+                    send(driver)
+                    add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+                    l_msg = last_msg(driver)
+                    l_name = last_msg_name(l_msg)
+                    l_date_unix = last_msg_date(l_msg)
+                    l_msg_text = last_msg_text(l_msg)
+                    add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+                    fl = True
+                elif l_msg_text == "нет":
+                    write(driver, "хорошо")
+                    send(driver)
+                    add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+                    l_msg = last_msg(driver)
+                    l_name = last_msg_name(l_msg)
+                    l_date_unix = last_msg_date(l_msg)
+                    l_msg_text = last_msg_text(l_msg)
+                    add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+                    fl = True
+                else:
+                    add_BD(series_collection, l_name, l_date_unix, l_msg_text)
+                date = l_date_unix
+
+                # else:
+                #     write("что?")
+            time.sleep(2)
             if fl:
                 f += 1
+        #
+        # driver.implicitly_wait(10)
 
-        # driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[4]/div[2]/div[4]/div[1]/button').click()
-        # print(btn_msg.text)
-        driver.implicitly_wait(10)
-
-        # last_msg = all_msg.find_element_by_class_name("im-mess--text")
-        # print(last_msg.get_attribute('innerHTML'))
 
         #screenshot
         #driver.save_screenshot('./image.png')
@@ -97,32 +162,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support.expected_conditions import presence_of_element_located
-#
-# #This example requires Selenium WebDriver 3.13 or newer
-# with webdriver.Chrome() as driver:
-#     wait = WebDriverWait(driver, 10)
-#     driver.get("https://google.com/ncr")
-#     driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
-#     first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>span")))
-#     print(first_result.get_attribute("textContent"))
-
-#
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-#
-# driver = webdriver.Chrome()
-#
-# # Navigate to url
-# driver.get("http://www.google.com")
-#
-# # Store 'google search' button web element
-# searchBtn = driver.find_element(By.LINK_TEXT, "Мне повезёт!")
-#
-# # Perform context-click action on the element
-# webdriver.ActionChains(driver).context_click(searchBtn).perform()
